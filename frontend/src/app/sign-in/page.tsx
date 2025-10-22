6890a3e79f0cd26e43bec603dc7ad2b1299de826
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-23 00:05:44 +0800

    feat: Created a signin page

---
+++ frontend/src/app/sign-in/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { SignIn } from "@stackframe/stack";
+
+export default function SignInPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignIn />
+    </div>
+  );
+}
