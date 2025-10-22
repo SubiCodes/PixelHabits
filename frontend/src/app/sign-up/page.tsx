b111d2b5244ea63b8183e31710e6448f47725dd0
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-23 00:06:09 +0800

    feat: Created a signup page

---
+++ frontend/src/app/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { SignUp } from "@stackframe/stack";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignUp />
+    </div>
+  );
+}
