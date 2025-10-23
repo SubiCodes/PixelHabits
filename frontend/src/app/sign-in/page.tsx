fea557fc604598a42cb855b5693393d01c2ab8d4
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-23 19:03:55 +0800

    feat: Completed the design for the sign in page

--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 import { SignIn } from "@stackframe/stack";
+import Image from "next/image";
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn />
+    <div className="flex flex-1 items-center justify-center min-h-screen p-4">
+      <div className="flex flex-col items-center justify-center w-full max-w-md sm:max-w-lg lg:max-w-xl bg-white border elevation-2 p-4 sm:p-6 lg:p-8 rounded-lg">
+        <div className="flex items-center justify-center w-full">
+          <Image
+            src="/logos/logo_with_tagline.png"
+            alt="Pixel Habits"
+            width={300}
+            height={300}
+            className="w-auto h-auto max-h-[30vh]"
+            priority
+          />
+        </div>
+        <div className="flex w-full items-center justify-center mt-4">
+          <SignIn />
+        </div>
+      </div>
     </div>
   );
 }
