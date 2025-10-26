02dbda6cee0d6601b0baa3dc1c7ec35aed8c0f02
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-26 22:22:19 +0800

    feat: Created a module for cloudinary

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HabitsModule } from './habits/habits.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [DatabaseModule, HabitsModule],
+  imports: [DatabaseModule, HabitsModule, CloudinaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
