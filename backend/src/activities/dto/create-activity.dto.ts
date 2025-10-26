faf21fd8d047ac6fab398099196cff3fab618931
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-26 22:44:59 +0800

    feat: Created the dto for the activities module

--- backend/src/activities/dto/create-activity.dto.ts
+++ backend/src/activities/dto/create-activity.dto.ts
@@ -1,1 +1,17 @@
-export class CreateActivityDto {}
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateActivityDto {
+  @IsString()
+  ownerId: string;
+
+  @IsString()
+  habitId: string;
+
+  @IsString()
+  @IsOptional()
+  caption?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isPublic?: boolean = false;
+}
--- backend/src/activities/dto/update-activity.dto.ts
+++ backend/src/activities/dto/update-activity.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
 import { CreateActivityDto } from './create-activity.dto';
 
-export class UpdateActivityDto extends PartialType(CreateActivityDto) {}
+export class UpdateActivityDto extends PartialType(CreateActivityDto) {
+  @IsString()
+  @IsOptional()
+  caption?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isPublic?: boolean;
+}
