05233a0d71204879327aefe5da14a2dc9a6ce631
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-26 22:45:59 +0800

    feat: Added the database module import for the activities module

--- backend/src/activities/activities.module.ts
+++ backend/src/activities/activities.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
 })
