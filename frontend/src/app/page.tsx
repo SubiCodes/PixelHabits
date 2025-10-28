061bb4e94906c029b907371949431dcb57ce4970
Rein Subisol <subisolreineerkim@gmail.com>
2025-10-28 21:39:34 +0800

    feat: Push user to home instead of root when they are logged in

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { HeroSection } from "@/components/sections/HeroSection";
 import { FeaturesSection } from "@/components/sections/FeaturesSection";
 import { HabitTrackerPreview } from "@/components/sections/HabitTrackerPreview";
 import { FYPShowcase } from "@/components/sections/FYPShowcase";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
+import { useUser } from "@stackframe/stack";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+  const user = useUser();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/home");
+    }
+  }, [user, router]);
+
   return (
     <div className="font-sans min-h-screen flex flex-col">
       <Navbar />
