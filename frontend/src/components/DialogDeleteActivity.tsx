97c4b257c88a7a0ab5ede6e6efc0b2fbea2bd4bf
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-08 17:10:20 +0800

    feat: Delete modal for the activity done

---
+++ frontend/src/components/DialogDeleteActivity.tsx
@@ -0,0 +1,62 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { ReactNode, useState } from "react"
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+import { Activity, useActivityStore } from "@/store/useActivityStore"
+
+interface DialogDeleteActivityProps {
+    trigger: ReactNode
+    activity: Activity
+    atHabitPage?: boolean
+}
+
+
+export function DialogDeleteActivity({ trigger, activity, atHabitPage }: DialogDeleteActivityProps) {
+    const [open, setOpen] = useState(false);
+    const router = useRouter();
+
+    const activityStore = useActivityStore();
+
+    const handleClose = () => {
+        setOpen(false)
+    };
+
+    const deleteActivity = async () => {
+        await activityStore.deleteActivity(activity.id);
+        if (atHabitPage) {
+            router.back();
+        }
+    }
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {trigger}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action will permanently delete your activity &quot;{activity.caption}&quot;? This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction className="bg-destructive active:bg-destructive/80 hover:bg-destructive hover:cursor-pointer" onClick={() => deleteActivity()}>
+                        Continue
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
