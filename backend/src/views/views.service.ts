8bc839f945019781bb33a5a483ceb49333c20f7b
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 21:42:09 +0800

    feat: Created remove function

--- backend/src/views/views.service.ts
+++ backend/src/views/views.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateViewDto } from './dto/create-view.dto';
-import { UpdateViewDto } from './dto/update-view.dto';
 import { DatabaseService } from 'src/database/database.service';
-import { createId } from '@paralleldrive/cuid2';
 
 @Injectable()
 export class ViewsService {
@@ -11,7 +9,6 @@ export class ViewsService {
   create(createViewDto: CreateViewDto) {
     return this.databaseService.views.create({
       data: {
-        id: createId(),
         ownerId: createViewDto.owner_id,
         activityId: createViewDto.activity_id,
       }
@@ -22,15 +19,9 @@ export class ViewsService {
     return `This action returns all views`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} view`;
-  }
-
-  update(id: number, updateViewDto: UpdateViewDto) {
-    return `This action updates a #${id} view`;
-  }
-
   remove(id: number) {
-    return `This action removes a #${id} view`;
+    this.databaseService.views.delete({
+      where: { id: id.toString() }
+    })
   }
 }
