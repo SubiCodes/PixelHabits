4db2158a5773e48e70f3f157e7289fa42df7f9c7
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 22:11:39 +0800

    feat: Created the resource for the comments module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,10 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { ActivitiesModule } from './activities/activities.module';
 import { ViewsModule } from './views/views.module';
 import { LikesModule } from './likes/likes.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [DatabaseModule, HabitsModule, CloudinaryModule, ActivitiesModule, ViewsModule, LikesModule],
+  imports: [DatabaseModule, HabitsModule, CloudinaryModule, ActivitiesModule, ViewsModule, LikesModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
