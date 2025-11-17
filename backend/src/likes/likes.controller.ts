686c2d985fc5372d3a2fd9f8ac26f5c694c3893e
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 22:06:45 +0800

    feat: Edited the controller for likes to match its service

--- backend/src/likes/likes.controller.ts
+++ backend/src/likes/likes.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './dto/create-like.dto';
-import { UpdateLikeDto } from './dto/update-like.dto';
 
 @Controller('likes')
 export class LikesController {
@@ -12,23 +11,8 @@ export class LikesController {
     return this.likesService.create(createLikeDto);
   }
 
-  @Get()
-  findAll() {
-    return this.likesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.likesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
-    return this.likesService.update(+id, updateLikeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.likesService.remove(+id);
+  @Get(':userId')
+  findAll(@Param('userId') userId: string) {
+    return this.likesService.findAll(userId);
   }
 }
