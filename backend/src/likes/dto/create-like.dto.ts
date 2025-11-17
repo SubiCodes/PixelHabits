b51220faa44ec5184ca55085fc4d6a983bfe6316
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 21:59:31 +0800

    feat: Created the DTO for the likes module

--- backend/src/likes/dto/create-like.dto.ts
+++ backend/src/likes/dto/create-like.dto.ts
@@ -1,1 +1,9 @@
-export class CreateLikeDto {}
+import { IsString } from "class-validator";
+
+export class CreateLikeDto {
+    @IsString()
+    owner_id: string;
+    
+    @IsString()
+    activity_id: string;
+}
