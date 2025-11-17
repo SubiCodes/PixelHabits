68dd36c0681384c0844b88aa8782eb3b40b575d9
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 22:18:18 +0800

    feat: Created the create function in the comments service that has a prevention for repeating comments on the same activity

--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createCommentDto: CreateCommentDto) {
+    const repeatedComment = await this.databaseService.comments.findFirst({
+      where: {
+        ownerId: createCommentDto.owner_id,
+        activityId: createCommentDto.activity_id,
+        commentText: createCommentDto.comment_text,
+      }
+    });
+
+    if (repeatedComment) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          error: 'BAD_REQUEST',
+          message: 'You have already posted this comment in this activity.',
+          timestamp: new Date().toISOString(),
+        },
+        HttpStatus.BAD_REQUEST
+      );
+    };
+
+    return this.databaseService.comments.create({
+      data: {
+        ownerId: createCommentDto.owner_id,
+        activityId: createCommentDto.activity_id,
+        commentText: createCommentDto.comment_text,
+      }
+    });
   }
 
   findAll() {
