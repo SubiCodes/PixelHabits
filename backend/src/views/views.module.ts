7bd7ebf87c9e8c1ec9c3e1ad05f61e142dc5de97
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 21:23:46 +0800

    feat: Added the databasse module to the imports of views

--- backend/src/views/views.module.ts
+++ backend/src/views/views.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ViewsService } from './views.service';
 import { ViewsController } from './views.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ViewsController],
   providers: [ViewsService],
 })
