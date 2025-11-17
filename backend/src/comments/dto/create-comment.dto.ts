eb65a6baa4843e47369e66ac38fc5f08d583386a
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 22:12:34 +0800

    feat: Created the DTO for the comments

--- backend/src/comments/dto/create-comment.dto.ts
+++ backend/src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCommentDto {}
+import { IsString, MaxLength } from "class-validator";
+
+export class CreateCommentDto {
+    @IsString()
+    owner_id: string;
+
+    @IsString()
+    activity_id: string;
+
+    @IsString()
+    @MaxLength(500)
+    comment_text: string;
+}
