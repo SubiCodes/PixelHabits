b257adacccecf26c8b4e09df5767aa0cf1b3584c
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-17 21:22:53 +0800

    feat: Updated the right DTO for the views

--- backend/src/views/dto/create-view.dto.ts
+++ backend/src/views/dto/create-view.dto.ts
@@ -1,1 +1,9 @@
-export class CreateViewDto {}
+import { IsString } from "class-validator";
+
+export class CreateViewDto {
+    @IsString()
+    owner_id: string;
+
+    @IsString()
+    activity_id: string;
+}
