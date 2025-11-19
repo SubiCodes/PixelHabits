03120b02ede8a85183569dff8e9c35c478cd967a
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-20 00:22:05 +0800

    feat: Created the controller called content to fetch activities using fastapi api

--- backend/src/contents/contents.controller.ts
+++ backend/src/contents/contents.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
+  
+  @Post('userId')
+  async getContents(@Query('userId') userId: string) {
+    return this.contentsService.getRecommendedContent(userId);
+  }
 }
