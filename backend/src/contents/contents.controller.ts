23153bf398a14c1cf05819b7a20a32eec9706f03
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-20 00:23:31 +0800

    fix: Used Param isntead of Query for easier calls

--- backend/src/contents/contents.controller.ts
+++ backend/src/contents/contents.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Post, Query } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
-  
+
   @Post('userId')
-  async getContents(@Query('userId') userId: string) {
+  async getContents(@Param('userId') userId: string) {
     return this.contentsService.getRecommendedContent(userId);
   }
 }
