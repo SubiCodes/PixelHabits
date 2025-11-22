2399f00e8c28aaa352e378180ed07cc3935c7f35
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-22 15:57:53 +0800

    feat: Created then resource for the activities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HabitsModule } from './habits/habits.module';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [DatabaseModule, HabitsModule],
+  imports: [DatabaseModule, HabitsModule, ActivitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
