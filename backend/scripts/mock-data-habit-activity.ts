26c01272358eab62c07ce7c06ebeedc01efeaff0
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-22 23:33:41 +0800

    feat: Added more medias and made it feel like self improvement centered

--- backend/scripts/mock-data-habit-activity.ts
+++ backend/scripts/mock-data-habit-activity.ts
@@ -31,15 +31,57 @@ export async function generateMockHabitActivityData(ownerIds: string[]) {
 
   // Example media URLs (royalty-free images/videos)
   const mediaSamples = [
-    'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
-    'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg',
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-    'https://images.unsplash.com/photo-1465101046530-73398c7f28ca',
-    'https://images.pexels.com/photos/34950/pexels-photo.jpg',
-    'https://cdn.pixabay.com/photo/2016/11/29/09/32/animal-1867121_1280.jpg',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308',
-    'https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg',
-    'https://cdn.pixabay.com/photo/2017/01/20/00/30/mountains-1998925_1280.jpg',
+    // Gym & Exercise
+    'https://images.unsplash.com/photo-1517836357463-d25dfeac3438', // gym weights
+    'https://images.unsplash.com/photo-1518611012118-696072aa579a', // running
+    'https://images.unsplash.com/photo-1506744038136-46273834b3fb', // yoga
+    'https://images.pexels.com/photos/2261487/pexels-photo-2261487.jpeg', // pushups
+    'https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg', // gym
+    'https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg', // cycling
+    'https://images.pexels.com/photos/375737/pexels-photo-375737.jpeg', // running shoes
+    'https://images.pexels.com/photos/260352/pexels-photo-260352.jpeg', // stretching
+    'https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg', // group exercise
+    'https://images.pexels.com/photos/1552106/pexels-photo-1552106.jpeg', // gym
+    // Coding & Tech
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c', // laptop coding
+    'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2', // code on screen
+    'https://images.unsplash.com/photo-1461749280684-dccba630e2f6', // developer workspace
+    'https://images.pexels.com/photos/1181675/pexels-photo-1181675.jpeg', // code
+    'https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg', // laptop
+    'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg', // coding
+    'https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg', // computer
+    'https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg', // code
+    'https://images.pexels.com/photos/1181359/pexels-photo-1181359.jpeg', // workspace
+    // Studying & Reading
+    'https://images.unsplash.com/photo-1513258496099-48168024aec0', // books
+    'https://images.unsplash.com/photo-1465101178521-c1a4c8a0a8b7', // reading
+    'https://images.unsplash.com/photo-1517841905240-472988babdf9', // study
+    'https://images.pexels.com/photos/256455/pexels-photo-256455.jpeg', // reading
+    'https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg', // books
+    'https://images.pexels.com/photos/261909/pexels-photo-261909.jpeg', // study
+    'https://images.pexels.com/photos/207662/pexels-photo-207662.jpeg', // library
+    'https://images.pexels.com/photos/267586/pexels-photo-267586.jpeg', // notebook
+    'https://images.pexels.com/photos/414519/pexels-photo-414519.jpeg', // desk
+    // Writing & Journaling
+    'https://images.unsplash.com/photo-1518717758536-85ae29035b6d', // writing
+    'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e', // journal
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80', // pen
+    'https://images.pexels.com/photos/210661/pexels-photo-210661.jpeg', // writing
+    'https://images.pexels.com/photos/261909/pexels-photo-261909.jpeg', // notebook
+    'https://images.pexels.com/photos/414519/pexels-photo-414519.jpeg', // desk
+    'https://images.pexels.com/photos/267586/pexels-photo-267586.jpeg', // notebook
+    'https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg', // books
+    // Miscellaneous Self-Improvement
+    'https://images.unsplash.com/photo-1465101046530-73398c7f28ca', // meditation
+    'https://images.pexels.com/photos/1552106/pexels-photo-1552106.jpeg', // gym
+    'https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg', // group exercise
+    'https://images.pexels.com/photos/375737/pexels-photo-375737.jpeg', // running shoes
+    'https://images.pexels.com/photos/260352/pexels-photo-260352.jpeg', // stretching
+    'https://images.pexels.com/photos/2261487/pexels-photo-2261487.jpeg', // pushups
+    'https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg', // gym
+    'https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg', // cycling
+    'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg', // yoga
+    'https://images.pexels.com/photos/34950/pexels-photo.jpg', // yoga
   ];
 
   let ownerIdx = 0;
