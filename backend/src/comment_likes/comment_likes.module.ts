44a8917ae8d5a6b144db5489af10c08a84019397
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-27 22:26:21 +0800

    feat: Added the databas module import in the comment_likes module

--- backend/src/comment_likes/comment_likes.module.ts
+++ backend/src/comment_likes/comment_likes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentLikesService } from './comment_likes.service';
 import { CommentLikesController } from './comment_likes.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CommentLikesController],
   providers: [CommentLikesService],
 })
