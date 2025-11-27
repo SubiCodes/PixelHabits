14f992b01722259741f94aa92150fe8cf833e142
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-27 22:36:44 +0800

    feat: Made the commen_like service for creating and destroying also made the id of comment_like auto generated

--- backend/src/comment_likes/comment_likes.service.ts
+++ backend/src/comment_likes/comment_likes.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentLikeDto } from './dto/create-comment_like.dto';
-import { UpdateCommentLikeDto } from './dto/update-comment_like.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class CommentLikesService {
-  create(createCommentLikeDto: CreateCommentLikeDto) {
-    return 'This action adds a new commentLike';
-  }
+  private constructor(private readonly databaseService: DatabaseService) { }
 
-  findAll() {
-    return `This action returns all commentLikes`;
-  }
+  async create(createCommentLikeDto: CreateCommentLikeDto) {
+    const existingLike = await this.databaseService.commentLikes.findFirst({
+      where: {
+        ownerId: createCommentLikeDto.owner_id,
+        commentId: createCommentLikeDto.comment_id,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} commentLike`;
-  }
-
-  update(id: number, updateCommentLikeDto: UpdateCommentLikeDto) {
-    return `This action updates a #${id} commentLike`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} commentLike`;
+    if (existingLike) {
+      return this.databaseService.commentLikes.delete({
+        where: {
+          ownerId_commentId: {
+            ownerId: createCommentLikeDto.owner_id,
+            commentId: createCommentLikeDto.comment_id,
+          }
+        }
+      })
+    } else {
+      return this.databaseService.commentLikes.create({
+        data: {
+          ownerId: createCommentLikeDto.owner_id,
+          commentId: createCommentLikeDto.comment_id,
+        }
+      })
+    }
   }
 }
