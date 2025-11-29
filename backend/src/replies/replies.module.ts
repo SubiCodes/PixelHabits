6b82d1d4462af43b4618ee6c2021fcbee5a8acf1
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-29 13:11:46 +0800

    feat: Added the database import for the replies module

--- backend/src/replies/replies.module.ts
+++ backend/src/replies/replies.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RepliesService } from './replies.service';
 import { RepliesController } from './replies.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [RepliesController],
   providers: [RepliesService],
+  imports: [DatabaseModule],
 })
 export class RepliesModule {}
