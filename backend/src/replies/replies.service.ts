8d4feae314e6a65a38d74bbf0741e2cb0467fc5c
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-29 13:25:28 +0800

    feat: returnd the reply with user data for after creation

--- backend/src/replies/replies.service.ts
+++ backend/src/replies/replies.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateReplyDto } from './dto/create-reply.dto';
 import { UpdateReplyDto } from './dto/update-reply.dto';
 import { DatabaseService } from 'src/database/database.service';
+import { enrichWithUserData } from 'src/common/utils/user-enrichment.util';
+import { serializeModelDates } from 'src/utils/serializeModelDates';
 
 @Injectable()
 export class RepliesService {
@@ -9,13 +11,16 @@ export class RepliesService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   async create(createReplyDto: CreateReplyDto) {
-    return await this.databaseService.replies.create({
+    const reply = await this.databaseService.replies.create({
       data: {
         ownerId: createReplyDto.owner_id,
         commentId: createReplyDto.comment_id,
         replyText: createReplyDto.reply_text,
       },
     });
+
+    const replyWithUserData = await enrichWithUserData(reply);
+    return await serializeModelDates([replyWithUserData])[0];
   }
 
   findAll(commentId: string) {
