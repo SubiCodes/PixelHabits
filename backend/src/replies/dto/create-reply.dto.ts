987279766e0324cf6a85d9efeead8f06e5033a86
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-29 13:11:15 +0800

    feat: Created the DTO for replies

--- backend/src/replies/dto/create-reply.dto.ts
+++ backend/src/replies/dto/create-reply.dto.ts
@@ -1,1 +1,13 @@
-export class CreateReplyDto {}
+import { IsString, MaxLength } from "class-validator";
+
+export class CreateReplyDto {
+    @IsString()
+    owner_id: string;
+
+    @IsString()
+    activity_id: string;
+
+    @IsString()
+    @MaxLength(500)
+    reply_text: string;
+}
