e4cb14a13967461784895e2570f65102fb96f69b
Rein Subisol <subisolreineerkim@gmail.com>
2025-11-29 13:13:30 +0800

    feat: Created the database service constructor for the replies service. wip: Create reply function

--- backend/src/replies/replies.service.ts
+++ backend/src/replies/replies.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReplyDto } from './dto/create-reply.dto';
 import { UpdateReplyDto } from './dto/update-reply.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class RepliesService {
-  create(createReplyDto: CreateReplyDto) {
-    return 'This action adds a new reply';
+
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createReplyDto: CreateReplyDto) {
+    const reply = await this.databaseService.replies.create({
+      data: {
+        id: crypto.randomUUID(),
+        ownerId: createReplyDto.owner_id,
+        activityId: createReplyDto.activity_id,
+        replyText: createReplyDto.reply_text,
+      },
+    });
   }
 
   findAll() {
