42a94e2a64207c32e5820472a33930d04cf35133
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-02 19:03:24 +0800

    feat: Changed the profile service to only contain functions for finding one user and udpating a user plus create an instance of the database service

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class ProfileService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
-  }
-
-  findAll() {
-    return `This action returns all profile`;
-  }
+  constructor(private readonly databaseService: DatabaseService) { }
 
   findOne(id: number) {
     return `This action returns a #${id} profile`;
@@ -20,7 +14,4 @@ export class ProfileService {
     return `This action updates a #${id} profile`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
-  }
 }
