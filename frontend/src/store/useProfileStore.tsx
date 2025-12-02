4d840e632b901fcb0d1442fe1a60d978af9ecda8
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-02 22:55:05 +0800

    feat: Created the base for the store for Profile

---
+++ frontend/src/store/useProfileStore.tsx
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+});
+
+interface ProfileStore {
+
+};
+
+export const useProfileStore = create<ProfileStore>((set) => ({
+    
+}));
