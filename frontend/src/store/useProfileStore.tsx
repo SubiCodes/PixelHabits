53f559a2707351d7576147ef6cef729fcc18198b
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-02 22:56:54 +0800

    feat: Created a user type

--- frontend/src/store/useProfileStore.tsx
+++ frontend/src/store/useProfileStore.tsx
@@ -5,6 +5,18 @@ const api = axios.create({
     baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
 });
 
+export interface User {
+    id: string;
+    rawJson: unknown;
+    name: string | null;
+    email: string | null;
+    createdAt: string;
+    updatedAt: string | null;
+    deletedAt: string | null;
+    bio: string | null;
+    isNew: boolean;
+}
+
 interface ProfileStore {
 
 };
