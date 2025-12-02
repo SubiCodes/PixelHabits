ee7ea57814619c75987cc16f952d8d0fe1659be7
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-02 19:01:39 +0800

    feat: Imported the database module to the profile module

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
