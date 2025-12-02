6e20bfea74874deeef4c91535800e349141124a1
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-02 23:30:33 +0800

    feat: Created the Dialog for changing the bio

---
+++ frontend/src/components/profile_components/DialogEditBio.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+
+interface DialogEditBioProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function DialogEditBio({ open, onOpenChange }: DialogEditBioProps) {
+  const handleClose = () => {
+    onOpenChange(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit Bio</DialogTitle>
+          <DialogDescription>
+            Update your profile bio below and click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        {/* <FormEditBio onSuccess={handleClose} /> */}
+      </DialogContent>
+    </Dialog>
+  )
+}
