21f77e0658b036157047377c44ad19bf252082a9
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-03 15:45:34 +0800

    feat: Added a condition to the tabs component to not display if empty string

--- frontend/src/components/profile_components/TabsProfilePages.tsx
+++ frontend/src/components/profile_components/TabsProfilePages.tsx
@@ -9,19 +9,21 @@ interface TabsProfilePagesProps {
 function TabsProfilePages({ pages, activeTab = pages[0], onChangeTab }: TabsProfilePagesProps) {
     return (
         <div className='w-full flex flex-row border-b'>
-            {pages.map((page, index) => (
-                <div 
-                    key={index} 
-                    className={`flex-1 text-center py-3 border-b-2 cursor-pointer transition-colors ${
-                        activeTab === page 
-                            ? 'border-primary text-primary' 
-                            : 'border-transparent hover:border-gray-300'
-                    }`}
-                    onClick={() => onChangeTab(page)}
-                >
-                    <span className='text-md font-medium'>{page}</span>
-                </div>
-            ))}
+            {pages.map((page, index) => 
+                page.trim() !== '' ? (
+                    <div 
+                        key={index} 
+                        className={`flex-1 text-center py-3 border-b-2 cursor-pointer transition-colors ${
+                            activeTab === page 
+                                ? 'border-primary text-primary' 
+                                : 'border-transparent hover:border-gray-300'
+                        }`}
+                        onClick={() => onChangeTab(page)}
+                    >
+                        <span className='text-md font-medium'>{page}</span>
+                    </div>
+                ) : null
+            )}
         </div>
     )
 }
