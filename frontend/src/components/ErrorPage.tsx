f1d77f23c96c136cecdf22803a322ea132137062
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-03 16:06:49 +0800

    feat: Created an error page reusable page

---
+++ frontend/src/components/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from './ui/button';
+
+interface ErrorPageProps {
+    errorMessage?: string;
+    retryAction?: () => void;
+}
+
+function ErrorPage({ errorMessage, retryAction }: ErrorPageProps) {
+  return (
+    <div className='flex flex-1 flex-col gap-2 items-center justify-center'>
+      <h1>{errorMessage ?? "An unexpected error occurred. Please try again."}</h1>
+      <Button onClick={retryAction}>Retry</Button>
+    </div>
+  )
+}
+
+export default ErrorPage
