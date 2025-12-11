72fbad86d20d608a7bf9f4cf9d7bd64ad35a61d3
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-11 23:51:47 +0800

    feat: Created the base variables for the new store

---
+++ frontend/src/store/useIndividualActivityStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand'
+import axios from 'axios';
+import { toast } from 'sonner';
+import { Activity } from './useActivityStore';
+
+const api = axios.create({
+    baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+});
+
+interface IndividualActivityStore {
+    activity: Activity | null
+    gettingActivity: boolean
+    getActivityById: (activityId: string) => Promise<void>
+}
+
+export const useIndividualActivityStore = create<IndividualActivityStore>((set) => ({
+    activity: null,
+    gettingActivity: false,
+    getActivityById: async (activityId: string) => {
+        try {
+            set({ gettingActivity: true });
+        } catch (err) {
+            if (axios.isAxiosError(err) && err.response?.data) {
+                const { message, suggestion } = err.response.data;
+                toast.error(message || 'Unable to get activity.', {
+                    id: 'get',
+                    description: suggestion || 'Please try again',
+                });
+            } else {
+                toast.error('Unable to get activity.', {
+                    id: 'get',
+                    description: 'Unable to get activity.',
+                });
+            }
+        } finally {
+            set({ gettingActivity: false });
+        }
+    }
+}))
