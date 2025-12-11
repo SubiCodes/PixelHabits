298cadd8647a2bf8e0bc1cc5381a78f8b0068371
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-11 13:57:57 +0800

    feat: Created a remove search function

--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -5,17 +5,17 @@ import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class SearchService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly databaseService: DatabaseService) { }
 
   async create(createSearchDto: CreateSearchDto) {
     const user = await this.databaseService.users_sync.findUnique({
       where: { id: createSearchDto.userId },
       select: { searches: true },
     });
-    
+
     const addedSearch = await this.databaseService.users_sync.update({
       where: { id: createSearchDto.userId },
-      data: { 
+      data: {
         searches: [...(user?.searches || []), ...createSearchDto.searches],
       },
     });
@@ -34,7 +34,20 @@ export class SearchService {
     return `This action updates a #${id} search`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} search`;
+  async remove(id: string, searchTerm: string) {
+    const user = await this.databaseService.users_sync.findUnique({
+      where: { id: id },
+      select: { searches: true },
+    });
+
+    const updatedSearches = (user?.searches || []).filter(term => term !== searchTerm);
+    
+    const updatedUser = await this.databaseService.users_sync.update({
+      where: { id: id },
+      data: {
+        searches: updatedSearches,
+      },
+    });
+    return updatedUser;
   }
 }
