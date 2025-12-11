2e2c520cc86a30cd0891fe5cff1e2ff24c33cfc3
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-11 14:00:55 +0800

    feat: Cleaned and corrected the search controller to match the service

--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Param, Delete } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { CreateSearchDto } from './dto/create-search.dto';
-import { UpdateSearchDto } from './dto/update-search.dto';
 
 @Controller('search')
 export class SearchController {
@@ -12,23 +11,8 @@ export class SearchController {
     return this.searchService.create(createSearchDto);
   }
 
-  @Get()
-  findAll() {
-    return this.searchService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.searchService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSearchDto: UpdateSearchDto) {
-    return this.searchService.update(+id, updateSearchDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.searchService.remove(+id);
+  remove(@Param('id') id: string, @Body('searchTerm') searchTerm: string) {
+    return this.searchService.remove(id, searchTerm);
   }
 }
