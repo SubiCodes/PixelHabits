f14df3b16ba6137388231323411046b3ab3598ca
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-11 15:38:58 +0800

    feat: Created the base for the store for search fuctions

---
+++ frontend/src/store/useSearchStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+});
+
+interface SearchStore {
+    getRecentSearches: (id: string) => Promise<void>
+    gettingRecentSearches: boolean
+    recentSearches: string[]
+}
+
+export const useSearchStore = create<SearchStore>((set) => ({
+    recentSearches: [],
+    gettingRecentSearches: false,
+    getRecentSearches: async (id: string) => {
+
+    },
+}));
