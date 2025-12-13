c68fb17cee7c067d560b9a00823514ac7b6bc4ee
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-13 16:40:35 +0800

    feat: Created the store function for getting the leaderboards data

--- frontend/src/store/useLeaderBoards.store.ts
+++ frontend/src/store/useLeaderBoards.store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import axios from 'axios';
+import { toast } from 'sonner';
 
 const api = axios.create({
     baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
@@ -25,8 +26,32 @@ export const useLeaderBoardStore = create<LeaderBoardStore>((set) => ({
     interactionLeaders: null,
     streakLeaders: null,
     gettingLeaderBoards: false,
-    gettingLeaderBoardsError: null, 
+    gettingLeaderBoardsError: null,
     getLeaderBoards: async () => {
-
+        try {
+            set({ gettingLeaderBoards: true, gettingLeaderBoardsError: null });
+            const res = await api.get('/leaderboards');
+            set({ 
+                interactionLeaders: res.data.interaction,
+                streakLeaders: res.data.streak,
+            });
+        } catch (err) {
+            if (axios.isAxiosError(err) && err.response?.data) {
+                const { message, suggestion } = err.response.data;
+                toast.error(message || 'Unable to get leaderboards.', {
+                    id: 'get',
+                    description: suggestion || 'Please try again later',
+                });
+                set({ gettingLeaderBoardsError: message || 'Unable to get leaderboards.' });
+            } else {
+                toast.error('Unable to get leaderboards.', {
+                    id: 'get',
+                    description: 'Unable to get leaderboards.',
+                });
+                set({ gettingLeaderBoardsError: 'Unable to get leaderboards.' });
+            }
+        } finally {
+            set({ gettingLeaderBoards: true });
+        }
     },
 }));
