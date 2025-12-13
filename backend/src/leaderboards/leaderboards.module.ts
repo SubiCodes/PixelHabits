bd719a34d7b090c1a2e2ab60ac5df433b74bda43
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-13 16:04:42 +0800

    feat: Added the database module import on the leaderboards module

--- backend/src/leaderboards/leaderboards.module.ts
+++ backend/src/leaderboards/leaderboards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LeaderboardsService } from './leaderboards.service';
 import { LeaderboardsController } from './leaderboards.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [LeaderboardsService],
   controllers: [LeaderboardsController]
 })
