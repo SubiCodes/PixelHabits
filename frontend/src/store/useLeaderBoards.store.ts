30f4ebae1bc0940b0d9f155c4d4ba56592772e4e
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-13 16:37:56 +0800

    feat: Created the types for the leaderboard stores

--- frontend/src/store/useLeaderBoards.store.ts
+++ frontend/src/store/useLeaderBoards.store.ts
@@ -5,6 +5,18 @@ const api = axios.create({
     baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
 });
 
+export interface LeaderBoardType {
+    id: string;
+    type: "interaction" | "streak";
+    userIds: string[];
+    amounts: number[];
+    updatedAt: string | Date;
+}
+
 interface LeaderBoardStore {
-    
+    interactionLeaders: LeaderBoardType | null
+    streakLeaders: LeaderBoardType | null
+    gettingLeaderBoards: boolean
+    getLeaderBoards: () => Promise<void>
+    gettingLeaderBoardsError: string | null
 }
