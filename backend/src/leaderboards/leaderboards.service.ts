a57f58a96a0c351b8972026a06cdad3232f42da0
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-13 16:07:24 +0800

    feat: Added the cron imports and injector

--- backend/src/leaderboards/leaderboards.service.ts
+++ backend/src/leaderboards/leaderboards.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class LeaderboardsService {
     constructor(private readonly databaseService: DatabaseService) { }
 
+    @Cron('0 0 * * *', {
+        timeZone: 'Asia/Manila',
+    })
+    async updateLeaderboards() {
+        console.log('Updating leaderboards at 12AM Philippine time');
+        // Calculation logic will go here
+    }
 }
